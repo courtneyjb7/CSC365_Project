92fa83f9d12b4650e747c796e39ec24bdd143998
Vee-Guz <vero.guzman48612@gmail.com>
2023-05-08 00:08:14 -0700

    included test classes for GET /classes/{class_id}

--- test/test_classes.py
+++ test/test_classes.py
@@ -2,9 +2,11 @@ from fastapi.testclient import TestClient
 
 from src.api.server import app
 
+import json
+
 client = TestClient(app)
 
-def add_classes_invalid_class_type(capsys):
+def test_add_classes_invalid_class_type(capsys):
     response = client.post("/classes/0", 
                             json=
                             {
@@ -40,37 +42,50 @@ def test_add_classes():
     assert response.text == '"Class added"'
 
 
-def add_attendance_existing():
-    response = client.post("/classes/7/0/attendance", 
-                            json=
-                            {
-                                "month": 5,
-                                "day": 26,
-                                "year": 2024,
-                                "start_hour": 12,
-                                "start_minutes": 30,
-                                "end_hour": 1,
-                                "end_minutes": 30,
-                                "class_type_id": 0
-                            })
+# TODO: how to test put method
+
+# def test_add_attendance_existing():
+#     response = client.put("/classes/7/0/attendance", 
+#                             json=
+#                             {
+#                                 "month": 5,
+#                                 "day": 26,
+#                                 "year": 2024,
+#                                 "start_hour": 12,
+#                                 "start_minutes": 30,
+#                                 "end_hour": 1,
+#                                 "end_minutes": 30,
+#                                 "class_type_id": 0
+#                             })
+#     assert response.status_code == 200
+
+#     assert response.text == '"Attendance updated"'
+
+
+# def test_add_attendance_new():
+#     response = client.put("/classes/4/1/attendance", 
+#                             json=
+#                             {
+#                                 "month": 5,
+#                                 "day": 26,
+#                                 "year": 2024,
+#                                 "start_hour": 12,
+#                                 "start_minutes": 30,
+#                                 "end_hour": 1,
+#                                 "end_minutes": 30,
+#                                 "class_type_id": 0
+#                             })
+#     assert response.status_code == 200
+
+#     assert response.text == '"Attendance updated"'
+
+def test_get_class():
+    response = client.get("/classes/2")
     assert response.status_code == 200
 
-    assert response.text == '"Attendance updated"'
+    with open("test/classes/2.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
 
-
-def add_attendance_new():
-    response = client.post("/classes/4/1/attendance", 
-                            json=
-                            {
-                                "month": 5,
-                                "day": 26,
-                                "year": 2024,
-                                "start_hour": 12,
-                                "start_minutes": 30,
-                                "end_hour": 1,
-                                "end_minutes": 30,
-                                "class_type_id": 0
-                            })
-    assert response.status_code == 200
-
-    assert response.text == '"Attendance updated"'
+def test_get_clas_400():
+    response = client.get("/classes/-1")
+    assert response.status_code == 404
