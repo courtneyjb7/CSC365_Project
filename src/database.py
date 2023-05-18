943322bd4e07679bbef32e4be1ace2430dbf14d7
courtneyjb7 <72003830+courtneyjb7@users.noreply.github.com>
2023-05-18 16:30:24 -0700

    env variables

--- src/database.py
+++ src/database.py
@@ -16,11 +16,11 @@ def try_parse(type, val):
 
 def database_connection_url():
     dotenv.load_dotenv()
-    DB_USER: str = os.environ.get("POSTGRES_USER")
-    DB_PASSWD = os.environ.get("POSTGRES_PASSWORD")
-    DB_SERVER: str = os.environ.get("POSTGRES_SERVER")
-    DB_PORT: str = os.environ.get("POSTGRES_PORT")
-    DB_NAME: str = os.environ.get("POSTGRES_DB")
+    DB_USER: str = os.environ.get("DEV_POSTGRES_USER")
+    DB_PASSWD = os.environ.get("DEV_POSTGRES_PASSWORD")
+    DB_SERVER: str = os.environ.get("DEV_POSTGRES_SERVER")
+    DB_PORT: str = os.environ.get("DEV_POSTGRES_PORT")
+    DB_NAME: str = os.environ.get("DEV_POSTGRES_DB")
     return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
 
 # Create a new DB engine based on our connection string
