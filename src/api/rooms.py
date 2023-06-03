c43fedab53f766cb93544e027dceb2e5e66134f4
courtneyjb7 <72003830+courtneyjb7@users.noreply.github.com>
2023-06-02 20:19:54 -0700

    get_room now returns name, max_cap, max_class_size

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -44,7 +44,8 @@ def get_room(
     it returns:
     - `room_id`: the id of the room in the facility that meets the class's needs
     - `room_name`: the name of the room
-    - `max_dog_capacity`: the maximum number of dogs that can attend the class
+    - `max_dog_capacity`: the maximum number of dogs that can fit in the room
+    - `max_class_size`: the max number of dogs that can attend a class of the given class type
     """
     try:
         with db.engine.connect() as conn:
@@ -72,7 +73,7 @@ def get_room(
             #                                     end_minutes)
 
             available_rooms = conn.execute(sqlalchemy.text("""
-                SELECT room_id, max_dog_capacity
+                SELECT room_id, max_dog_capacity, room_name
                 FROM rooms
                 WHERE room_id NOT IN (
                     SELECT classes.room_id
@@ -92,21 +93,33 @@ def get_room(
                     }]).fetchall()
             
             if available_rooms != []:
+                #get available rooms with max dog capacity > class max 
                 holds_class_max = list(filter(lambda x: x[1] >= class_max, 
                                               available_rooms))
                 # avail rooms already sorted
                 if len(holds_class_max):
                     # select room_id with smallest capacity 
                     # that fits class max if one exists
-                    room = holds_class_max[0][0]
+                    # room = holds_class_max[0][0]
+                    room = {
+                        "room_id": holds_class_max[0][0],
+                        "room_name": holds_class_max[0][2],
+                        "max_dog_capacity": holds_class_max[0][1],
+                        "max_class_size": class_max
+                    }
                     return room
                 else: 
                     # if no room that hold class max size, select largest room
-                    room = available_rooms[-1]
+                    # room = available_rooms[-1]
+                    room = {
+                        "room_id": available_rooms[-1][0],
+                        "room_name": available_rooms[-1][2],
+                        "max_dog_capacity": available_rooms[-1][1],
+                        "max_class_size": class_max
+                    }
                     raise HTTPException(status_code=404, 
                             detail=f"""the only rooms available have a max room \
-capacity < given class max size. The largest room available has room_id \
-{room[0]} of capacity {room[1]}""")
+capacity < given class max size. The largest room available is {room}""")
             else:
                 raise HTTPException(status_code=404, 
                                     detail="no rooms available for this date/time.")
